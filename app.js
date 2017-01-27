72f091feeb69a2965913458a53b6d8774de09883
Alejandro Del Papa <adelpapa@sparkdigital.com>
2017-01-27 11:35:58 -0300

    Added routes to scripts

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-/*var helmet = require('helmet');*/
+var helmet = require('helmet');
 var bodyParser = require('body-parser');
 
 var i18n_options = {
@@ -20,6 +20,17 @@ var index = require('./routes/index');
 var api = require('./api/api');
 
 var app = express();
+//Helmet set one by one
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.frameguard({ action: 'sameorigin' }));
+app.disable('x-powered-by');
+var sixtyDaysInSeconds = 5184000;
+app.use(helmet.hsts({
+  maxAge: sixtyDaysInSeconds
+}));
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,12 +43,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-/*app.use(helmet);*/
 
 app.use(i18n.middleware);
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/scripts/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/scripts/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/scripts/angular', express.static(__dirname + '/node_modules/angular/'));
+app.use('/scripts/angular-route', express.static(__dirname + '/node_modules/angular-route/'));
 app.use('*', index);
 
 // catch 404 and forward to error handler
